60fed13a8abc087a30c19b27ed1cbee3f936f0f2
KimHoseong <ghtjd000129@naver.com>
2018-06-28 19:57:40 +0900

    첫 커밋

---
+++ Assets/Scripts/BattleStart.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleStart : MonoBehaviour {
+    public int cNum;
+    public int pNum;
+    Vector3 p_location = new Vector3(0f, 0f, 0f);
+    Vector3 p_rotation = new Vector3(0f, 0f, 0f);
+    public GameObject[] Charac = new GameObject[7];
+    GameObject temp;
+    public int startHP;
+
+	// Use this for initialization
+	void Start () {
+        cNum = 1;       //for test
+
+        p_location = gameObject.transform.position;
+
+        if (pNum == 1)
+            p_rotation = new Vector3(3f, 3f, 1f);
+
+        if (pNum == 2)
+            p_rotation = new Vector3(-3f, 3f, 1f);
+
+        if (cNum == 1)
+        {
+            GameObject temp = Instantiate(Charac[0], p_location, Quaternion.identity);
+            temp.transform.localScale = p_rotation;
+            temp.GetComponent<InputKey>().isplayer = pNum;
+            startHP = 3;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/InputKey.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputKey : MonoBehaviour {
+
+    public float speed;
+    public int isplayer;
+    float spd;
+
+	// Use this for initialization
+	void Start () {
+        spd = speed * Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isplayer == 1)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(0f, spd * 20f, 0f);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(0f, spd * -20f, 0f);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(spd * -20f, 0f, 0f);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(spd * 20f, 0f, 0f);
+            }
+        }
+	}
+}
