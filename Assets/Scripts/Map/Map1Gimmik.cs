0169574a2515f1e4bcfadd11c2ad390f6ff48ec6
KimHoseong <ghtjd000129@naver.com>
2018-07-05 21:52:52 +0900

    진규야 받아라

--- Assets/Scripts/Map/Map1Gimmik.cs
+++ Assets/Scripts/Map/Map1Gimmik.cs
@@ -7,11 +7,11 @@ public class Map1Gimmik : MonoBehaviour {
 
     GameObject[] map1Bullet = new GameObject[2];
     GameObject map1RazerCross;
+    GameObject map1RazerVisual0;
     GameObject map1RazerVisual1;
     GameObject map1RazerVisual2;
 
     GameObject map1RazerDetector1;
-    GameObject map1RazerDetector2;
 
     // Use this for initialization
     void Start () {
@@ -19,10 +19,10 @@ public class Map1Gimmik : MonoBehaviour {
         map1Bullet[0] = Resources.Load("Map/Map1Bullet1") as GameObject;
         map1Bullet[1] = Resources.Load("Map/Map1Bullet2") as GameObject;
         map1RazerCross = Resources.Load("Map/Map1RazerCross") as GameObject;
+        map1RazerVisual0 = Resources.Load("Map/Map1RazerVisual0") as GameObject;
         map1RazerVisual1 = Resources.Load("Map/Map1RazerVisual1") as GameObject;
         map1RazerVisual2 = Resources.Load("Map/Map1RazerVisual2") as GameObject;
         map1RazerDetector1 = Resources.Load("Map/Map1RazerDetector1") as GameObject;
-        map1RazerDetector2 = Resources.Load("Map/Map1RazerDetector2") as GameObject;
 
         Debug.Log("mapNum : " + mapNum);
 
@@ -40,7 +40,7 @@ public class Map1Gimmik : MonoBehaviour {
 
     IEnumerator Map1GimmikFunction()
     {
-        yield return new WaitForSeconds(10f);
+        yield return new WaitForSeconds(1f);
 
         //StartCoroutine(Map1Gimmik1());
         StartCoroutine(Map1Gimmik2());
@@ -126,22 +126,55 @@ public class Map1Gimmik : MonoBehaviour {
         GameObject map1RazerCrossTemp;
         GameObject map1RazerDetectorTemp1;
         GameObject map1RazerDetectorTemp2;
-        GameObject map1RazerVisualTemp1;
-        GameObject map1RazerVisualTemp2;
+        GameObject map1RazerVisual1Temp1;
+        GameObject map1RazerVisual1Temp2;
+        GameObject map1RazerVisual2Temp1;
+        GameObject map1RazerVisual2Temp2;
+        GameObject map1RazerVisual0Temp1;
+        GameObject map1RazerVisual0Temp2;
 
-        map1RazerCrossTemp = Instantiate(map1RazerCross, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        map1RazerCrossTemp = Instantiate(map1RazerCross, new Vector3(Random.Range(-400f, 400f), Random.Range(-250f, 250f), -1f), Quaternion.identity);
+        map1RazerVisual0Temp1 = Instantiate(map1RazerVisual0, map1RazerCrossTemp.transform.position, Quaternion.identity);
+        map1RazerVisual0Temp2 = Instantiate(map1RazerVisual0, map1RazerCrossTemp.transform.position, Quaternion.AngleAxis(90f, Vector3.forward));
 
         yield return new WaitForSeconds(1f);
 
-        map1RazerVisualTemp1 = Instantiate(map1RazerVisual1, new Vector3(0f, 0f, 0f), Quaternion.identity);
-        map1RazerVisualTemp2 = Instantiate(map1RazerVisual2, new Vector3(0f, 0f, 0f), Quaternion.identity);
-        map1RazerDetectorTemp1 = Instantiate(map1RazerDetector1, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        Destroy(map1RazerVisual0Temp1);
+        Destroy(map1RazerVisual0Temp2);
+        map1RazerVisual2Temp1 = Instantiate(map1RazerVisual2, map1RazerCrossTemp.transform.position, Quaternion.identity);
+        map1RazerVisual2Temp2 = Instantiate(map1RazerVisual2, map1RazerCrossTemp.transform.position, Quaternion.AngleAxis(90f, Vector3.forward));
+
+        yield return new WaitForSeconds(0.05f);
+
+        Destroy(map1RazerVisual2Temp1);
+        Destroy(map1RazerVisual2Temp2);
+        map1RazerVisual1Temp1 = Instantiate(map1RazerVisual1, map1RazerCrossTemp.transform.position, Quaternion.identity);
+        map1RazerVisual1Temp2 = Instantiate(map1RazerVisual1, map1RazerCrossTemp.transform.position, Quaternion.AngleAxis(90f, Vector3.forward));
+        map1RazerDetectorTemp1 = Instantiate(map1RazerDetector1, map1RazerCrossTemp.transform.position, Quaternion.identity);
         map1RazerDetectorTemp1.GetComponent<BulletIdentifier>().isPlayer_Bullet = 3;
         map1RazerDetectorTemp1.GetComponent<BulletIdentifier>().isDestroiable = false;
-        map1RazerDetectorTemp2 = Instantiate(map1RazerDetector2, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        map1RazerDetectorTemp2 = Instantiate(map1RazerDetector1, map1RazerCrossTemp.transform.position, Quaternion.AngleAxis(90f, Vector3.forward));
         map1RazerDetectorTemp2.GetComponent<BulletIdentifier>().isPlayer_Bullet = 3;
         map1RazerDetectorTemp2.GetComponent<BulletIdentifier>().isDestroiable = false;
 
+        yield return new WaitForSeconds(1f);
+
+        Destroy(map1RazerVisual1Temp1);
+        Destroy(map1RazerVisual1Temp2);
+        map1RazerVisual2Temp1 = Instantiate(map1RazerVisual2, map1RazerCrossTemp.transform.position, Quaternion.identity);
+        map1RazerVisual2Temp2 = Instantiate(map1RazerVisual2, map1RazerCrossTemp.transform.position, Quaternion.AngleAxis(90f, Vector3.forward));
+
+        yield return new WaitForSeconds(0.05f);
+
+        Destroy(map1RazerVisual2Temp1);
+        Destroy(map1RazerVisual2Temp2);
+        map1RazerVisual0Temp1 = Instantiate(map1RazerVisual0, map1RazerCrossTemp.transform.position, Quaternion.identity);
+        map1RazerVisual0Temp2 = Instantiate(map1RazerVisual0, map1RazerCrossTemp.transform.position, Quaternion.AngleAxis(90f, Vector3.forward));
+
+        yield return new WaitForSeconds(0.05f);
+
+        Destroy(map1RazerVisual0Temp1);
+        Destroy(map1RazerVisual0Temp2);
     }
 
 }
