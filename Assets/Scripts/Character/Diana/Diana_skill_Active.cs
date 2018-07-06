2d3418809dc3eebc4de68e29948f21687ab035ad
이진규 <wlsrb22222@naver.com>
2018-07-06 14:21:37 +0900

    Revert "Revert "다이아나 스킬 4 컷인과 시스템 구현""
    This reverts commit b7ed4abeeb2601867bee7b7bcb556a71eed9b382.

--- Assets/Scripts/Character/Diana/Diana_skill_Active.cs
+++ Assets/Scripts/Character/Diana/Diana_skill_Active.cs
@@ -7,11 +7,15 @@ public class Diana_skill_Active : MonoBehaviour {
 
 	int isPlayer_C;
 	int skillNum;
-
+	GameObject wing;
 	GameObject L_P1;
 	GameObject L_P2;
+	public GameObject DianaSkillCutIn;
+	public bool skill4_end=false;
 	// Use this for initialization
 	void Start () {
+		wing = GameObject.Find ("Wing");
+		wing.SetActive (false);
 		isPlayer_C = gameObject.GetComponent<InputKey>().isPlayer;
 		L_P1 = GameObject.Find("L_P1Start");
 		L_P2 = GameObject.Find("L_P2Start");
@@ -20,7 +24,7 @@ public class Diana_skill_Active : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		skillNum = gameObject.GetComponent<HPManagement>().skillTrigger;
-		if (skillNum == 1)
+		if (GetComponent<HPManagement>().use==true&&skillNum == 1)
 		{
 			gameObject.GetComponent<Diana_passive>().skill1();
 		}
@@ -31,6 +35,15 @@ public class Diana_skill_Active : MonoBehaviour {
 		if (skillNum == 3)
 		{
 			gameObject.GetComponent<Diana_skill3>().skill3();
+		}
+		if (skillNum == 4)
+		{
+			wing.SetActive (true);
+			gameObject.GetComponent<Diana_skill4>().skill4();
+		}
+		if (skill4_end == true) {
+			wing.SetActive (false);
+			skill4_end = false;
 		}
 		gameObject.GetComponent<HPManagement>().skillTrigger = 0;
 	}
